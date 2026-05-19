859a5f06a63193a315d93a1e1c0a35ffbfd6e4bb
Alirio Castro <aliriocastro@gmail.com>
2026-05-18 23:10:57 -0400

    feat(core): enums (SourceType, ScanTerminator, OutputMode, SourceConnectionState, SerialEncoding)

---
+++ src/AutolumoBarcodeScannerTool.Core/Models/Enums.cs
@@ -0,0 +1,38 @@
+namespace AutolumoBarcodeScannerTool.Core.Models;
+
+public enum SourceType
+{
+    Serial,
+    HidKeyboard
+}
+
+public enum ScanTerminator
+{
+    Cr,
+    Lf,
+    CrLf,
+    Any
+}
+
+public enum OutputMode
+{
+    Tab,
+    TabEnter,
+    TabOnly,
+    Custom
+}
+
+public enum SourceConnectionState
+{
+    Disconnected,
+    Connecting,
+    Connected,
+    Error
+}
+
+public enum SerialEncoding
+{
+    Ascii,
+    Utf8,
+    Latin1
+}
